a23284b70fc210abb8413020526e67f4fad5c1b2
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2022-05-17 20:45:59 -0300

    Using mongo model on kafka event consumer

---
+++ src/brocker/events.consumer.ts
@@ -0,0 +1,32 @@
+import { Kafka, EachMessagePayload } from "kafkajs";
+
+import Event, { IEvent } from "../models/event.model";
+
+const kafka = new Kafka({
+  clientId: "nl.events.consumer",
+  brokers: [process.env.BROCKER_HOST || "localhost:9092"],
+});
+
+export const start = async () => {
+  const consumer = kafka.consumer({ groupId: "nl.events.consumer" });
+
+  await consumer.connect();
+  await consumer.subscribe({ topic: "nl.tp.events", fromBeginning: true });
+
+  await consumer.run({
+    partitionsConsumedConcurrently: 1,
+    eachMessage: async (payload: EachMessagePayload) => {
+      const { message } = payload;
+      if (!message) return;
+
+      const messageValue = Buffer.from(
+        message.value?.toString() || ""
+      ).toString();
+      if (!messageValue) return;
+
+      const event: IEvent = JSON.parse(messageValue);
+      const eventModel = new Event(event);
+      await eventModel.save();
+    },
+  });
+};
--- src/brocker/index.ts
+++ src/brocker/index.ts
@@ -1,7 +1,7 @@
-import requestBrocker from "./request";
+import { start as eventsConsumerStarter } from "./events.consumer";
 
 export default class KafkaBrocker {
   async start(): Promise<any> {
-    await requestBrocker.start();
+    await eventsConsumerStarter();
   }
 }
