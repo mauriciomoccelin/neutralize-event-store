ca7b48980b6d7fa8d4b28acfe06d86775e718de4
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2022-05-18 21:53:06 -0300

    Usin env variable to net mongo uri

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/eventstore");
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/event-store';
+if (!mongoUri) throw new Error("No mongo uri provided");
 
+mongoose.connect(mongoUri);
 export default mongoose;
