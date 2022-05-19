a3d7a8941469e69e113ac836d81ec449a53726a6
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2022-05-18 21:53:58 -0300

    refactoring graphql event resolver

--- src/graphql/resolvers/events.ts
+++ src/graphql/resolvers/events.ts
@@ -1,52 +1,65 @@
 import Event, { IEvent } from "../../models/event.model";
 
-interface IPageRequest {
-  limit: number;
-  offset: number;
+class PageRequest {
+  limit!: number;
+  offset!: number;
   type?: string;
-  datetime: string;
+  datetime!: string;
+
+  static normalize(input: PageRequest): PageRequest {
+    const maxItemsResult = 10;
+
+    input.datetime = new Date(input.datetime).toJSON();
+    input.offset = input.offset <= 0 ? 1 : input.offset;
+    input.limit = input.limit > maxItemsResult ? maxItemsResult : input.limit;
+
+    return input;
+  }
 }
 
-const getEvents = async (input: IPageRequest) => {
-  const { limit, offset, type, datetime } = input;
+const toItemResponse = (event: IEvent | any = {}) => {
+  if (!event) return null;
+
+  const item = {
+    type: event.type,
+    dateTime: `${event.dateTime.toJSON()}`,
+    aggregateId: event.aggregateId,
+    data: JSON.stringify(event.data),
+    metadata: JSON.stringify(event.metadata),
+  };
+
+  return item;
+};
+
+const getEvents = async (input: PageRequest) => {
+  const { limit, offset, type, datetime } = PageRequest.normalize(input);
 
   const queryCount = Event.count();
-
   type && queryCount.where("type", type);
-  type && queryCount.where("dateTime", { $gte: datetime });
+  queryCount.where("dateTime", { $gt: datetime });
 
   const queryItems = Event.find();
+  queryItems.skip(offset);
+  queryItems.limit(limit);
+  queryItems.sort({ dateTime: -1 });
 
-  type && queryCount.where("type", type);
-  type && queryCount.where("dateTime", { $gte: datetime });
-
-  limit > 30 ? queryItems.limit(30) : queryItems.limit(limit);
-
-  queryCount.skip(offset);
-  queryCount.sort({ dateTime: -1 });
+  type && queryItems.where("type", type);
+  queryItems.where("dateTime", { $gt: datetime });
 
   const total = await queryCount.exec();
   const items = await queryItems.exec();
 
   const result = {
     total,
-    items: items.map((item) => ({
-      type: item.type,
-      dateTime: `${item.dateTime.toJSON()}`,
-      aggregateId: item.aggregateId,
-      data: JSON.stringify(item.data),
-      metadata: JSON.stringify(item.metadata),
-    })),
+    items: items.map((event) => toItemResponse(event)),
   };
-
-  console.log(result);
 
   return result;
 };
 
 const getEventById = async ({ id = "" }) => {
-  const event = await Event.findById({ _id: id, aggregateId: id });
-  return event;
+  const event = await Event.findOne({ aggregateId: id });
+  return toItemResponse(event);
 };
 
 const newEvent = async (input: IEvent) => {
