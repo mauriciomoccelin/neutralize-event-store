1ecf2fe302d9a91fa813c7b0d71f51c9507a33ab
Mauricio Moccelin <25235995+mauriciomoccelin@users.noreply.github.com>
2022-08-02 22:27:39 -0300

    Fix documentation

--- build/run.ps1
+++ build/run.ps1
@@ -7,18 +7,18 @@ Clear-Host
 
 if ($True -eq $Teste2e) {
   Set-Location ../docker
-  docker-compose --env-file ../config/e2e.env up --build graphql-logs-e2e
+  docker-compose --env-file ../config/e2e.env up --build event-store-logs-e2e
   Set-Location ../build
 
   return
 } elseif ($True -eq $Dev) {
   Set-Location ../docker
-  docker-compose --env-file ../config/dev.env up --build graphql-logs-dev
+  docker-compose --env-file ../config/dev.env up --build event-store-logs-dev
   Set-Location ../build
 
   return
 }
 
 Set-Location ../docker
-docker-compose --env-file ../prod.env up --build --detach graphql-logs-api
+docker-compose --env-file ../prod.env up --build --detach event-store-logs-api
 Set-Location ../build
